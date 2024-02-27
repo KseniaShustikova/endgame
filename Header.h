d6e2ded2bd8dd9cfb7514c9af32dce1da54aafda
polittraaa <149580306+polittraaa@users.noreply.github.com>
2024-02-27 20:29:11 +0100

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include <stdio.h>
+#include <time.h>
+
+#define GRAVITY 0.3f;
+
+// cube
+typedef struct
+{
+	float x, y;
+	float dx, dy;
+	char* name;
+	int onLedge;
+} object;
+
+//платформа (по анології з об'єктом)
+typedef struct
+{
+	float x, y, w, h;
+} Block;
+
+// to simplify use
+typedef struct
+{
+	object cube;
+	Block block[100];
+	SDL_Renderer* renderer;
+} GameState;
+
+void processEvents(SDL_Window* wind, GameState* game);
+void doRnder(SDL_Renderer* renderer, GameState* game);
+void collisionDetect(GameState* game);
+void process(GameState* game);
+void loadGame(GameState* game);
