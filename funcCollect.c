e6a8f58899b92c3e20ab7df8dedfd3d2b798042d
Ksenia <200.ksenia.200@gmail.com>
2024-02-28 19:41:40 +0100

    changing the messages

--- funcCollect.c
+++ funcCollect.c
@@ -18,27 +18,28 @@ void SetCollectableImage(Collectable *element, SDL_Renderer *renderer, char* pat
 }
 // to set the text on top of the counter
 void SetCounterText(counter  *counter, SDL_Renderer *renderer ){
-    char text[20] ;
+    char text[100] ;
     char counterText[2];
     SDL_Color black = {0, 0, 0, 255};
     TTF_Font* Sans;
     SDL_Surface* surfaceMessage;
-    SDL_Texture* Message;
-
-    sprintf(counterText,"%d",counter->counter) ;
-    sprintf(text, "%s %s", "items collected: ", counterText); 
-    Sans = TTF_OpenFont("./resource/fonts/font.ttf", 12);
+    SDL_Texture* texture;
+    int texW = 0;
+    int texH = 0;
+    sprintf(counterText,"%d",counter->counter);
+    sprintf(text, "%s %s", "кількість зібраних предметів: ", counterText); 
+    Sans = TTF_OpenFont("./resource/fonts/font.ttf", 24);
     if(!Sans)
         printf("%s", TTF_GetError());
-    surfaceMessage = TTF_RenderText_Solid(Sans, text, black);
+    surfaceMessage =  TTF_RenderUTF8_Blended_Wrapped(Sans, text, black, 400);
     if(!surfaceMessage)
         printf("%s", TTF_GetError()); 
-    Message = SDL_CreateTextureFromSurface(renderer, surfaceMessage);
-    if(!Message)
+    texture = SDL_CreateTextureFromSurface(renderer, surfaceMessage);
+    if(!texture)
         printf("%s", TTF_GetError()); 
-
-    counter->rect = (SDL_Rect){0, 0, 200, 30};
-    counter->texture = Message;
+    SDL_QueryTexture(texture, NULL, NULL, &texW, &texH);
+    counter->rect = (SDL_Rect){0, 0, texW, texH};
+    counter->texture = texture;
 }
 
 void DisplayLetterText(SDL_Renderer *renderer){
@@ -48,12 +49,12 @@ void DisplayLetterText(SDL_Renderer *renderer){
     SDL_Texture* texture; 
     int texW = 0;
     int texH = 0;
-    int marginX = 200;
+    int marginX = 150;
     int marginY = 100;
     SDL_Rect background;
 
     msg.text = "Привіт, Алісо! Ласкаво просимо до Країни Чудес! Ми вже чекаємо на тебе на нашому святковому чаюванні, але, здається, ми загубили кілька предметів. Знайди їх, будь ласка, щоб злий Оракл не вийшов на наш слід! Він може зловити тебе, будь обережною!";
-    Sans = TTF_OpenFont("./resource/fonts/font.ttf", 20);
+    Sans = TTF_OpenFont("./resource/fonts/font.ttf", 24);
     if(!Sans)
         printf("%s", TTF_GetError());
     //replace 640 with window width
