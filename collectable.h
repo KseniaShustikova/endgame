7412acde0c16af8fe409f6cbd560d3cde7e2c400
Ksenia <200.ksenia.200@gmail.com>
2024-02-28 13:07:55 +0100

    added functions to a new file and modified the header

--- collectable.h
+++ collectable.h
@@ -1,8 +1,13 @@
+#pragma once
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+//#include "funcCollect.c"
 #include "./resource/SDL2/inc/SDL.h"
 #include "./resource/SDL_TTF/include/SDL2/SDL_ttf.h"
+
+//counter collectableCounter;
 
 typedef struct counter{
     int counter;
@@ -16,3 +21,9 @@ typedef struct collectable {
     SDL_Texture *image;
     bool isCollected;
 }Collectable;
+
+Collectable makeCollectable ( int x, int y, int width, int height);
+void SetCollectableImage(Collectable *element, SDL_Renderer *renderer, char* path);
+void SetCounterText(counter  *counter, SDL_Renderer *renderer );
+void renderCollectable (Collectable *element, SDL_Renderer *renderer, SDL_Rect *player );
+void displayText (SDL_Renderer *renderer, counter *counter);
