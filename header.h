7a314fe26cd13efe65f4b0ffe1969e2bd14ae89c
polittraaa <149580306+polittraaa@users.noreply.github.com>
2024-03-01 19:29:59 +0100

    Update header.h

--- header.h
+++ header.h
@@ -59,6 +59,7 @@ typedef struct
 	int levelcount;
 	SDL_Texture* aliceFrames[6];
 	SDL_Texture* tiles;
+        SDL_Texture* holetexture;
 
 	int time;
 
@@ -74,7 +75,7 @@ SDL_Texture* loadTexture(SDL_Renderer* renderer, const char* filename);
 void toggleSound();
 void handleEvents(SDL_Renderer* renderer, Button* startButton, Button* exitButton, Button* volumeButton, int* quit);
 void render(SDL_Renderer* renderer, SDL_Texture* backgroundTexture, Button* startButton, Button* exitButton, Button* volumeButton);
-void cleanup(SDL_Texture* startButtonTexture, SDL_Texture* exitButtonTexture, SDL_Texture* backgroundTexture, SDL_Texture* volumeOn, SDL_Texture* volumeOff, SDL_Renderer* renderer, SDL_Window* window);
+void cleanup(SDL_Texture* startButtonTexture, SDL_Texture* exitButtonTexture, SDL_Texture* backgroundTexture, SDL_Texture* volumeOn, SDL_Texture* volumeOff,SDL_Texture* holetexture, SDL_Renderer* renderer, SDL_Window* window);
 
 
 #endif
