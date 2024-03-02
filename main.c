390044cca1584c1d800897e4f8acc45a513f36d5
krutoinapas <150392452+krutoinapas@users.noreply.github.com>
2024-03-02 02:48:03 +0100

    переходы работают менюшка тоже

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include "header.h"
+#include "../inc/header.h"
 
 int main() {
 
@@ -7,19 +7,22 @@ int main() {
 	SDL_Init(SDL_INIT_VIDEO);
 	Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
 
-	SDL_Window* window = SDL_CreateWindow("EndGame", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_W, WINDOW_H, SDL_WINDOW_SHOWN);
+	SDL_Window* window = SDL_CreateWindow("Alice's Adventure: Tea Odyssey", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_W, WINDOW_H, SDL_WINDOW_SHOWN);
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 	game.renderer = renderer;
 	SDL_Surface* temp_surf = NULL;
 	SDL_Texture* backgroundTexture = loadTexture(renderer, "images/menu_b.bmp");
+	const char* gameBackground = "images/level0.bmp";
+	const char* gameMusic = "audio/level0.wav";
 	SDL_RenderClear(renderer);
 	SDL_RenderPresent(renderer);
 	SDL_Delay(300);
 
-	SDL_Texture* startButtonTexture = loadTexture(renderer, "buttons/start.bmp");
-	SDL_Texture* exitButtonTexture = loadTexture(renderer, "buttons/quit.bmp");
-	SDL_Texture* volumeOn = loadTexture(renderer, "buttons/vol1.bmp");
-	SDL_Texture* volumeOff = loadTexture(renderer, "buttons/vol2.bmp");
+	SDL_Texture* startButtonTexture = loadTexture(renderer, "images/start.bmp");	
+	SDL_Texture* exitButtonTexture = loadTexture(renderer, "images/quit.bmp");
+	SDL_Texture* volumeOn = loadTexture(renderer, "images/vol1.bmp");
+	SDL_Texture* volumeOff = loadTexture(renderer, "images/vol2.bmp");
+
 
 	game.startButton.texture = startButtonTexture;
 	game.startButton.rect.x = 518;
@@ -34,7 +37,7 @@ int main() {
 	game.exitButton.rect.h = 90;
 
 	game.volumeButton.texture = volumeOn;
-	game.volumeButton.rect.x = 1100;
+	game.volumeButton.rect.x = 1185;
 	game.volumeButton.rect.y = 50;
 	game.volumeButton.rect.w = 45;
 	game.volumeButton.rect.h = 45;
@@ -58,21 +61,39 @@ int main() {
 	}
 	if (start == 1)
 	{
-		loadGame(&game);
+		load_lvl_0(&game, gameBackground, gameMusic);
 
 		int done = 0;
+
+		//старый цикл
+		
+		while (!done)
+		{
+			//Check for events
+			done = processEvents(window, &game);
+			process(&game);
+			collisionDetect(&game);
+			//Render display
+			doRender(renderer, &game);
+			//don't burn up the CPU
+			SDL_Delay(16);
+		}  
+
+		load_lvl_1(&game, gameMusic);
+
+		done = 0;
 
 		//Event loop
 		while (!done)
 		{
 			//Check for events
-			done = processEvents(window, &game);
+			done = diana_processEvents(window, &game);
 
-			process(&game);
-			collisionDetect(&game);
+			diana_process(&game);
+			diana_collisionDetect(&game);
 
 			//Render display
-			doRender(renderer, &game);
+			diana_doRender(renderer, &game);
 
 			//don't burn up the CPU
 			SDL_Delay(16);
