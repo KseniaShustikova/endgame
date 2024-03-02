9d42537ab355f30dc3c4001c424f57656f9da21f
krutoinapas <150392452+krutoinapas@users.noreply.github.com>
2024-03-02 02:48:56 +0100

    переходы работают менюшка тоже

--- header.h
+++ header.h
@@ -1,7 +1,10 @@
+#pragma once
+
 #ifndef HEADER_H
 #define HEADER_H
 
 #include <SDL.h>
+#include <SDL_ttf.h>
 #include <SDL_image.h>
 #include <SDL_mixer.h>
 #include <stdio.h>
@@ -9,16 +12,15 @@
 #include <stdlib.h>
 #include <time.h>
 
+#pragma warning(disable : 4996)
+
 #define WINDOW_W 1280
 #define WINDOW_H 720
 
 #define FPS 60
 #define FRAME_TIME (1000 / FPS)
 
-#define GRAVITY 0.35f;
-
-const int DOOR_HEIGHT = 68;
-const int DOOR_WIDTH = 52;
+#define GRAVITY 0.4f;
 
 typedef struct {
 	SDL_Texture* texture;
@@ -40,8 +42,14 @@ typedef struct
 typedef struct Door
 {
 	int x, y, w, h;
-	bool isOpen;
 } Door;
+
+typedef struct Collectable
+{
+	int x, y, w, h;
+	bool isCollected;
+} Collectable;
+
 typedef struct
 {
 	int x, y, w, h;
@@ -51,29 +59,65 @@ typedef struct
 {
 	object alice;
 	Button startButton, exitButton, volumeButton;
-
 	Ledge ledges[100];
-	Door hole;
-	int levelcount;
+
+	Door door;
+
+	Collectable collectable;
+	int collactableCounter;
+	int levelCounter;
+
+	Mix_Music* backgroundMusic;
+
 	SDL_Texture* aliceFrames[6];
 	SDL_Texture* tiles;
-        SDL_Texture* holetexture;
+	SDL_Texture* doorImage;
+	SDL_Texture* doorImageLevel3;
+	SDL_Texture* collactbleImage;
+	SDL_Texture* backgroundTexture;
+	SDL_Texture* backgroundLeve1Texture;
+	SDL_Texture* backgroundLevel2Texture;
+	SDL_Texture* backgroundLevel3Texture;
+	SDL_Texture* texture; //end
 
 	int time;
 
 	SDL_Renderer* renderer;
+	bool isEnd;
 } GameState;
 
+int isAliceRichedCollectable(object* alice, Collectable* collectable);
+bool isAliceRichedTheDoor(object* alice, Door* door);
+void clearLedges(GameState* game);
+void initLevel2Door(GameState* game);
+void initLevel2Collectable(GameState* game);
+void initLevel3Ledges(GameState* game);
+void initLevel2Ledges(GameState* game);
+void renderCallectable(SDL_Renderer* renderer, GameState* game);
+void renderCounterText(int counter, SDL_Renderer* renderer);
+void DisplayEndPicture(SDL_Renderer* renderer, char* path, GameState* game);
+int collide2d(float x1, float y1, float x2, float y2, float wt1, float ht1, float wt2, float ht2);
+
+void teleportAliceToStartPosition(object* alice);
+bool isAliceOnTheFloor(object* alice);
+void diana_doRender(SDL_Renderer* renderer, GameState* game);
+void diana_process(GameState* game);
+int diana_processEvents(SDL_Window* window, GameState* game);
+void load_lvl_1(GameState* game);
+void initCollectableAboveLedge(GameState* game);
 int processEvents(SDL_Window* wind, GameState* game);
 void doRender(SDL_Renderer* renderer, GameState* game);
 void collisionDetect(GameState* game);
 void process(GameState* game);
-void loadGame(GameState* game);
+void load_lvl_0(GameState* game, const char* gameBackground, const char* gameMusic);
+void load_lvl_1(GameState* game, const char* musicFile);
+void diana_collisionDetect(GameState* game);
 SDL_Texture* loadTexture(SDL_Renderer* renderer, const char* filename);
 void toggleSound();
-void handleEvents(SDL_Renderer* renderer, Button* startButton, Button* exitButton, Button* volumeButton, int* quit);
+void handleEvents(SDL_Renderer* renderer, Button* startButton, Button* exitButton, Button* volumeButton, int* quit, int* start);
 void render(SDL_Renderer* renderer, SDL_Texture* backgroundTexture, Button* startButton, Button* exitButton, Button* volumeButton);
-void cleanup(SDL_Texture* startButtonTexture, SDL_Texture* exitButtonTexture, SDL_Texture* backgroundTexture, SDL_Texture* volumeOn, SDL_Texture* volumeOff,SDL_Texture* holetexture, SDL_Renderer* renderer, SDL_Window* window);
+void cleanup(SDL_Texture* startButtonTexture, SDL_Texture* exitButtonTexture, SDL_Texture* backgroundTexture, SDL_Texture* volumeOn, SDL_Texture* volumeOff, SDL_Renderer* renderer, SDL_Window* window);
+
 
 
 #endif
